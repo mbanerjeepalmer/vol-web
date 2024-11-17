34875c0f60e28ec278fc63d12801fb08f6bf418a
Maurice <mbanerjeepalmer@gmail.com>
2024-11-17 10:34:53 +0000

    history and homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
-	import InteractionHistory from '$lib/components/InteractionHistory.svelte';
+
+	let inputValue = '';
+	let inputElement: HTMLInputElement;
 
 	const predefinedPrompts = [
-		'I want to be like Mark Zuckerberg',
-		'I want to learn about AI',
-		'I enjoy football',
-		'I just graduated from my engineering degree'
+		'I want to be like',
+		'I was just wondering about',
+		'My one weird skill is',
+		'I yap too much about'
 	];
 </script>
 
-<div class="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-4 px-4 sm:grid-cols-2">
-	{#each predefinedPrompts as prompt}
-		<form method="POST" class="h-full">
+<div class="mx-auto mb-24 mt-16">
+	<div class="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-4 px-4 sm:grid-cols-2">
+		{#each predefinedPrompts as prompt}
 			<button
-				value={prompt}
-				name="prompt"
-				type="submit"
+				type="button"
+				on:click={() => {
+					inputValue = prompt + ' ';
+					inputElement.focus();
+				}}
 				class="h-full w-full text-wrap rounded-md border border-input p-3 hover:bg-accent hover:text-accent-foreground"
 				>{prompt}</button
 			>
-		</form>
-	{/each}
-</div>
-<form method="POST" class="mx-auto w-full max-w-lg px-4">
-	<div class="flex flex-col gap-2 sm:flex-row">
-		<Input name="prompt" class="w-full" placeholder="I'm thinking about..." />
-		<Button type="submit" class="w-full sm:w-auto">Submit</Button>
+		{/each}
 	</div>
-</form>
-<div class="mx-auto mt-28 max-w-lg px-4">
-	<InteractionHistory />
+	<form method="POST" class="mx-auto w-full max-w-lg px-4">
+		<div class="flex flex-col gap-2 sm:flex-row">
+			<Input
+				bind:this={inputElement}
+				name="prompt"
+				bind:value={inputValue}
+				class="w-full"
+				placeholder="I'm thinking about..."
+			/>
+			<Button type="submit" class="w-full sm:w-auto">Submit</Button>
+		</div>
+	</form>
 </div>
+
+<a href="/history" class="mx-auto block text-center underline">my vols</a>
---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,3 @@
+<div class="mx-auto mt-28 max-w-lg px-4 opacity-90">
+	<InteractionHistory />
+</div>
