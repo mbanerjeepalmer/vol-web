aac835d0cb50b135bccbe92fea64728eee84a6ba
Maurice <mbanerjeepalmer@gmail.com>
2024-11-17 11:44:46 +0000

    history

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import InteractionHistory from '$lib/components/InteractionHistory.svelte';
+</script>
+
 <div class="mx-auto mt-28 max-w-lg px-4 opacity-90">
 	<InteractionHistory />
 </div>
