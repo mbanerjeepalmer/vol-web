72241ac0e2d2d6b99c274250e52006062b37a395
Maurice <mbanerjeepalmer@gmail.com>
2024-11-20 18:19:20 +0000

    fix nullable ratings

--- src/lib/components/Ratings.svelte
+++ src/lib/components/Ratings.svelte
@@ -3,7 +3,7 @@
 	import { getAverageRating } from '$lib/utils';
 	import { Compass, Sprout, GraduationCap, Gem } from 'lucide-svelte';
 
-	export let ratings: EpisodeRatings;
+	export let ratings: EpisodeRatings | undefined;
 
 	function getRatingColour(rating: number): string {
 		if (rating >= 80) return 'text-green-400';
@@ -13,14 +13,18 @@
 	}
 </script>
 
-<ul class="flex flex-row items-center justify-center gap-3 py-4">
-	<li><Compass class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.goal)}`} /></li>
-	<li><GraduationCap class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.context)}`} /></li>
-	<li><Gem class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.quality)}`} /></li>
-	<li><Sprout class="mr-1 inline h-4 w-4" /></li>
-	<li>
-		<span class={`font-black ${getRatingColour(getAverageRating(ratings))}`}>
-			{getAverageRating(ratings)}
-		</span>
-	</li>
-</ul>
+{#if ratings}
+	<ul class="flex flex-row items-center justify-center gap-3 py-4">
+		<li><Compass class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.goal)}`} /></li>
+		<li><GraduationCap class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.context)}`} /></li>
+		<li><Gem class={`mr-1 inline h-4 w-4 ${getRatingColour(ratings.quality)}`} /></li>
+		<li><Sprout class="mr-1 inline h-4 w-4" /></li>
+		<li>
+			<span class={`font-black ${getRatingColour(getAverageRating(ratings))}`}>
+				{getAverageRating(ratings)}
+			</span>
+		</li>
+	</ul>
+{:else}
+	<p>rating this episode</p>
+{/if}
