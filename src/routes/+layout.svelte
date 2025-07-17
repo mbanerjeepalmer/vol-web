eae9e6dad174469c1d82119e82b5431ccdcfdb2f
Maurice <mbanerjeepalmer@gmail.com>
2025-07-17 15:50:43 +0100

    footer at the bottom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,44 +14,48 @@
 	}
 </script>
 
-{#if $page.url.pathname !== '/'}
-	<header class="container mx-auto p-4 lg:mt-8">
-		<div class="flex items-center justify-center">
-			<a href="/">
-				<h1
-					class="inline-block bg-gradient-to-r from-fuchsia-500 to-green-500 bg-clip-text text-center text-3xl font-bold text-transparent"
-				>
-					vol
-				</h1>
-			</a>
-		</div>
-		<ul class="mx-auto mb-6 mt-4 flex w-full max-w-lg flex-row justify-end gap-4 text-sm"></ul>
-	</header>
-{/if}
+<div class="flex min-h-screen flex-col">
+	{#if $page.url.pathname !== '/'}
+		<header class="container mx-auto p-4 lg:mt-8">
+			<div class="flex items-center justify-center">
+				<a href="/">
+					<h1
+						class="inline-block bg-gradient-to-r from-fuchsia-500 to-green-500 bg-clip-text text-center text-3xl font-bold text-transparent"
+					>
+						vol
+					</h1>
+				</a>
+			</div>
+			<ul class="mx-auto mb-6 mt-4 flex w-full max-w-lg flex-row justify-end gap-4 text-sm"></ul>
+		</header>
+	{/if}
 
-{@render children()}
+	<main class="flex-grow">
+		{@render children()}
+	</main>
 
-<footer class="container mx-auto mt-16 max-w-lg p-4">
-	<ul class="mx-auto flex w-fit flex-row items-center justify-center gap-4 text-sm">
-		<li class="underline decoration-slate-400 opacity-70 hover:opacity-100">
-			<a href="/how-to">how to vol</a>
-		</li>
-		<li class="underline decoration-slate-400 opacity-70 hover:opacity-100">
-			{#if chatting}
-				<Chat />
-			{:else}
-				<Button
-					class="font light underline decoration-slate-400 opacity-70 hover:opacity-100"
-					variant="link"
-					on:click={() => (chatting = true)}>give feedback</Button
+	<footer class="container mx-auto mt-auto max-w-lg p-4">
+		<ul class="mx-auto flex w-fit flex-row items-center justify-center gap-4 text-sm">
+			<li class="underline decoration-slate-400 opacity-70 hover:opacity-100">
+				<a href="/how-to">how to vol</a>
+			</li>
+			<li class="underline decoration-slate-400 opacity-70 hover:opacity-100">
+				{#if chatting}
+					<Chat />
+				{:else}
+					<Button
+						class="font light underline decoration-slate-400 opacity-70 hover:opacity-100"
+						variant="link"
+						on:click={() => (chatting = true)}>give feedback</Button
+					>
+				{/if}
+			</li>
+			<li>
+				<a
+					href="https://tally.so/r/mJVbb7"
+					class="underline decoration-slate-400 opacity-70 hover:opacity-100">stay in touch</a
 				>
-			{/if}
-		</li>
-		<li>
-			<a
-				href="https://tally.so/r/mJVbb7"
-				class="underline decoration-slate-400 opacity-70 hover:opacity-100">stay in touch</a
-			>
-		</li>
-	</ul>
-</footer>
+			</li>
+		</ul>
+	</footer>
+</div>
